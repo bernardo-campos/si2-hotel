a5cd67549ec991047c04f3531d23c79eaec5aa5d
Bernardo <camposbg@gmail.com>
2021-08-31 16:33:57 -0300

    creating some client views

--- resources/views/client/reservations/index.blade.php
+++ resources/views/client/reservations/index.blade.php
@@ -1,13 +1,47 @@
 @extends('adminlte::page')
 
-@section('title', 'Reservas')
+@section('title', 'Mis Reservas')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Reservas</h1>
+    <div class="d-flex">
+        <h1 class="m-0 text-dark">Mis Reservas</h1>
+    </div>
 @stop
 
+@php
+    $heads = [
+        'Id',
+        'N° Res',
+        'Nº Tarjeta',
+        'F. Ingreso',
+        'F. Egreso',
+        // 'Huésped',
+        '#personas',
+        'Serv. Adic.', // (camas y cunas adicionales, estacionamiento)
+        'Estado', // (vigente, cancelada, expirada)
+    ];
+@endphp
+
 @section('content')
-    Listado de reservas
+
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+
+                <x-adminlte-datatable id="table1" :heads="$heads">
+                    @foreach([] as $row)
+                        <tr>
+                            @foreach($row as $cell)
+                                <td>{!! $cell !!}</td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </x-adminlte-datatable>
+
+            </div>
+        </div>
+    </div>
+
 @stop
 
 @push('js')
