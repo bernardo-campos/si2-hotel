2920ee1f8ba817c942a5fbdf44f6a08032c8b1ec
Bernardo <camposbg@gmail.com>
2021-08-31 23:58:59 -0300

    install select2

--- config/adminlte.php
+++ config/adminlte.php
@@ -400,7 +400,7 @@ return [
             ],
         ],
         'Select2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
