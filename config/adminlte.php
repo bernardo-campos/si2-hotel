9dca547825072d668f13071f70051d7cd677e2b1
Bernardo <camposbg@gmail.com>
2021-10-01 02:02:33 -0300

    style: changed class container-fluid

--- config/adminlte.php
+++ config/adminlte.php
@@ -125,7 +125,7 @@ return [
     'classes_brand' => '',
     'classes_brand_text' => '',
     'classes_content_wrapper' => '',
-    'classes_content_header' => 'container-fluid d-flex justify-content-between',
+    'classes_content_header' => 'container d-flex justify-content-between',
     'classes_content' => '',
     'classes_sidebar' => 'sidebar-dark-primary elevation-4',
     'classes_sidebar_nav' => '',
