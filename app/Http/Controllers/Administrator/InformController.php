d8ca019a1ca955469c83afbc2c0e3688b5126fcb
Bernardo <camposbg@gmail.com>
2021-10-27 15:19:35 -0300

    feat: payments report

--- app/Http/Controllers/Administrator/InformController.php
+++ app/Http/Controllers/Administrator/InformController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Administrator;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class InformController extends Controller
@@ -19,7 +21,19 @@ class InformController extends Controller
 
     public function payments()
     {
-        return view('admin.reports.payments');
+        $incomingsByMonth = Payment::whereYear('created_at', '=', date('Y'))
+            ->get()
+            ->groupBy( function($payment) {
+                return Carbon::parse($payment->created_at)->isoFormat('MMMM');
+            })->map( function($payments, $index) {
+                $total = $payments->reduce( fn ($carry, $payment) => $carry + $payment->ammount, 0.0);
+                return $total;
+            })->toArray();
+
+        return view('admin.reports.payments', [
+            'months' => array_keys($incomingsByMonth),
+            'incomings' => array_values($incomingsByMonth),
+        ]);
     }
 
     public function occupation()
--- resources/views/admin/reports/payments.blade.php
+++ resources/views/admin/reports/payments.blade.php
@@ -1,5 +1,7 @@
 @extends('adminlte::page')
 
+@section('plugins.Chartjs', true)
+
 @section('title', 'Informe de Pagos')
 
 @section('content_header')
@@ -7,10 +9,31 @@
 @stop
 
 @section('content')
-    Informe de Pagos
+    <div class="card">
+        <div class="card-header">Ingresos por mes del año actual ({{ date('Y') }})</div>
+        <div class="card-body">
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
 @stop
 
 @push('js')
+<script type="text/javascript">
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: {!! json_encode($months) !!},
+            datasets: [{
+                label: "Ingresos por mes",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: {!! json_encode($incomings) !!},
+            }]
+        },
+        options: {}
+    });
+</script>
 @endpush
 
 @push('css')
